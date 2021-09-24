213402784ce9a481336da9e6aed0685e1ee5e354
NormanWeinert <norman.weinert@med.uni-goettingen.de>
2021-09-24 11:36:41 +0200

    Add more Impfstoff + Smyptoms

--- src/Templates/ImpfstatusResources/Impfstoff.java
+++ src/Templates/ImpfstatusResources/Impfstoff.java
@@ -12,7 +12,7 @@ public class Impfstoff {
 
         BasicWriteCommands.Indents(writer,4);
 
-        int randomNum = ThreadLocalRandom.current().nextInt(1, 10 + 1);
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 42 + 1);
 
         switch (randomNum) {
             case 1:
@@ -74,6 +74,198 @@ public class Impfstoff {
                 writer.newLine();
                 BasicWriteCommands.Indents(writer, 4);
                 writer.write("\"display\": \"Haemophilus influenzae Type b + recombinant hepatitis B virus vaccine (product)\",");
+                break;
+            case 11:
+                writer.write("\"code\": \"787859002:{127489000=428126001}{127489000=396424005}{127489000=412375000}\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Vaccine product (product): { Has active ingredient (attribute) = Diphtheria vaccine (substance) } { Has active ingredient (attribute) = Hepatitis B virus vaccine (substance) } { Has active ingredient (attribute) = Tetanus vaccine (substance) }\",");
+                break;
+            case 12:
+                writer.write("\"code\": \"414004005\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Diphtheria + tetanus + pertussis + poliomyelitis + haemophilus influenzae b vaccine (product)\",");
+                break;
+            case 13:
+                writer.write("\"code\": \"4426081003\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Diphtheria + tetanus + pertussis + recombinant hepatitis B virus vaccine (product)\",");
+                break;
+            case 14:
+                writer.write("\"code\": \"787859002:{127489000=412374001}{127489000=396436004}\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Vaccine product (product): { Has active ingredient (attribute) = Haemophilus influenzae type b vaccine (substance) } { Has active ingredient (attribute) = Live Poliovirus vaccine (substance) }\",");
+                break;
+            case 15:
+                writer.write("\"code\": \"787859002:{127489000=428126001}{127489000=396438003}{127489000=412375000}\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Vaccine product (product): { Has active ingredient (attribute) = Diphtheria vaccine (substance) } { Has active ingredient (attribute) = Rubella vaccine (substance) } { Has active ingredient (attribute) = Tetanus vaccine (substance) }\",");
+                break;
+            case 16:
+                writer.write("\"code\": \"414005006\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Diphtheria + tetanus + pertussis + poliomyelitis vaccine (product)\",");
+                break;
+            case 17:
+                writer.write("\"code\": \"414006007\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Diphtheria + tetanus + poliomyelitis vaccine (product)\",");
+                break;
+            case 18:
+                writer.write("\"code\": \"419550004\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Measles + mumps + rubella + varicella vaccine (product)\",");
+                break;
+            case 19:
+                writer.write("\"code\": \"787859002:{127489000=396427003}{127489000=396438003}\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Vaccine product (product): { Has active ingredient (attribute) = Measles vaccine (substance) } { Has active ingredient (attribute) = Rubella vaccine (substance) }\",");
+                break;
+            case 20:
+                writer.write("\"code\": \"61153008\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Measles, mumps and rubella vaccine (product)\",");
+                break;
+            case 21:
+                writer.write("\"code\": \"785865001\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Measles and mumps vaccine (product)\",");
+                break;
+            case 22:
+                writer.write("\"code\": \"333702001\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Hepatitis A+B vaccine (product)\",");
+                break;
+            case 23:
+                writer.write("\"code\": \"350327004\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Diphtheria + tetanus vaccine (product)\",");
+                break;
+            case 24:
+                writer.write("\"code\": \"78785900:{127489000=398730001}{127489000=412374001}\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Vaccine product (product): { Has active ingredient (attribute) = Pneumococcal vaccine (substance) } { Has active ingredient (attribute) = Haemophilus influenzae type b vaccine (substance) }\",");
+                break;
+            case 25:
+                writer.write("\"code\": \"423912009\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Haemophilus influenzae type b + Meningococcal group C vaccine (product)\",");
+                break;
+            case 26:
+                writer.write("\"code\": \"787859002:{127489000=412374001}{127489000=396433007}{127489000=396411005}\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Vaccine product (product): { Has active ingredient (attribute) = Haemophilus influenzae type b vaccine (substance) } { Has active ingredient (attribute) = Pertussis vaccine (substance) } { Has active ingredient (attribute) = Toxoid (substance) }\",");
+                break;
+            case 27:
+                writer.write("\"code\": \"787859002:{127489000=396422009}{127489000=396441007}\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Vaccine product (product): { Has active ingredient (attribute) = Cholera vaccine (substance) } { Has active ingredient (attribute) = Typhoid vaccine (substance) }\",");
+                break;
+            case 28:
+                writer.write("\"code\": \"333697005\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Japanese B encephalitis vaccine\",");
+                break;
+            case 29:
+                writer.write("\"code\": \"333699008\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Tick-borne encephalitis vaccine\",");
+                break;
+            case 30:
+                writer.write("\"code\": \"90043005\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Mumps live virus vaccine\",");
+                break;
+            case 31:
+                writer.write("\"code\": \"111164008\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Poliovirus vaccine\",");
+                break;
+            case 32:
+                writer.write("\"code\": \"116077000\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Rotavirus vaccine\",");
+                break;
+            case 33:
+                writer.write("\"code\": \"333606008\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Rabies vaccine\",");
+                break;
+            case 34:
+                writer.write("\"code\": \"407737004\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Varicella-zoster vaccine\",");
+                break;
+            case 35:
+                writer.write("\"code\": \"33234009\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Smallpox vaccine\",");
+                break;
+            case 36:
+                writer.write("\"code\": \"424519000\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Human papillomavirus vaccine\",");
+                break;
+            case 37:
+                writer.write("\"code\": \"56844000\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Yellow fever vaccine\",");
+                break;
+            case 38:
+                writer.write("\"code\": \"386013003\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Rubella vaccine\",");
+                break;
+            case 39:
+                writer.write("\"code\": \"386012008\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Measles vaccine\",");
+                break;
+            case 40:
+                writer.write("\"code\": \"14745005\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Hepatitis A virus vaccine\",");
+                break;
+            case 41:
+                writer.write("\"code\": \"34689006\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Hepatitis B virus vaccine\",");
+                break;
+            case 42:
+                writer.write("\"code\": \"46233009\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Influenza virus vaccine\",");
                 break;
         }
 
--- src/Templates/SymptomResources/Code.java
+++ src/Templates/SymptomResources/Code.java
@@ -12,7 +12,7 @@ public class Code {
 
         BasicWriteCommands.Indents(writer,4);
 
-        int randomNum = ThreadLocalRandom.current().nextInt(1, 20 + 1);
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 40 + 1);
 
         switch (randomNum) {
             case 1:
@@ -134,6 +134,126 @@ public class Code {
                 writer.newLine();
                 BasicWriteCommands.Indents(writer, 4);
                 writer.write("\"display\": \"Loss of sense of smell (finding)\",");
+                break;
+            case 21:
+                writer.write("\"code\": \"36955009\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Loss of taste (finding)\",");
+                break;
+            case 22:
+                writer.write("\"code\": \"30746006\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Lymphadenopathy (disorder)\",");
+                break;
+            case 23:
+                writer.write("\"code\": \"367391008\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Malaise (finding)\",");
+                break;
+            case 24:
+                writer.write("\"code\": \"68962001\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Muscle pain (finding)\",");
+                break;
+            case 25:
+                writer.write("\"code\": \"68235000\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Nasal congestion (finding)\",");
+                break;
+            case 26:
+                writer.write("\"code\": \"64531003\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Nasal discharge (finding)\",");
+                break;
+            case 27:
+                writer.write("\"code\": \"422587007\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Nausea (finding)\",");
+                break;
+            case 28:
+                writer.write("\"code\": \"162397003\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Pain in throat (finding)\",");
+                break;
+            case 29:
+                writer.write("\"code\": \"38880002\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Rigor (finding)\",");
+                break;
+            case 30:
+                writer.write("\"code\": \"91175000\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Seizure (finding)\",");
+                break;
+            case 31:
+                writer.write("\"code\": \"46742003\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Skin ulcer (disorder)\",");
+                break;
+            case 32:
+                writer.write("\"code\": \"282145008\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Unable to walk (finding)\",");
+                break;
+            case 33:
+                writer.write("\"code\": \"422400008\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Vomiting (disorder)\",");
+                break;
+            case 34:
+                writer.write("\"code\": \"56018004\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Wheezing (finding)\",");
+                break;
+            case 35:
+                writer.write("\"code\": \"426000000\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Fever greater than 100.4 Fahrenheit / 38° Celsius (finding)\",");
+                break;
+            case 36:
+                writer.write("\"code\": \"13791008\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Asthenia (finding)\",");
+                break;
+            case 37:
+                writer.write("\"code\": \"22253000\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Pain (finding)\",");
+                break;
+            case 38:
+                writer.write("\"code\": \"28743005\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Productive cough (finding)\",");
+                break;
+            case 39:
+                writer.write("\"code\": \"11833005\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Dry cough (finding)\",");
+                break;
+            case 40:
+                writer.write("\"code\": \"40917007\",");
+                writer.newLine();
+                BasicWriteCommands.Indents(writer, 4);
+                writer.write("\"display\": \"Clouded consciousness (finding)\",");
                 break;
         }
     }
